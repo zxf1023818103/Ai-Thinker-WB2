7c0c20efa9eb9d8420111e4120b9cceadccd5d48
Zeng Xiangfei <zxf1023818103@hotmail.com>
2022-12-02 19:34:40 +0800

    暂存代码

--- applications/iot-solution/panel/panel/ui.c
+++ applications/iot-solution/panel/panel/ui.c
@@ -4,11 +4,14 @@
 #include <task.h>
 #include <blog.h>
 #include "bl_sys.h"
-#include "lvgl.h"
 #include "lv_conf.h"
+#include "lv_font.h"
 #include "lv_port_disp.h"
 #include "lv_port_indev.h"
+#include "lvgl.h"
 #include <hosal_timer.h>
+
+extern const lv_font_t FontAwesomePro;
 
 TaskHandle_t ui_task_handle;
 
@@ -16,39 +19,46 @@ static void create_ui(void)
 {
     lv_obj_t * tv = lv_tileview_create(lv_scr_act());
 
-    /*Tile1: just a label*/
-    lv_obj_t * tile1 = lv_tileview_add_tile(tv, 0, 0, LV_DIR_BOTTOM);
-    lv_obj_t * label = lv_label_create(tile1);
-    lv_label_set_text(label, "Scroll down");
-    lv_obj_center(label);
+    {
+        lv_obj_t * tile = lv_tileview_add_tile(tv, 0, 0, LV_DIR_BOTTOM);
+        lv_obj_set_layout(tile, LV_LAYOUT_GRID);
 
+        static lv_coord_t column_dsc[] = {LV_GRID_CONTENT, LV_GRID_FR(1), LV_GRID_TEMPLATE_LAST};
+        static lv_coord_t row_dsc[] = {LV_GRID_FR(1), LV_GRID_FR(1), LV_GRID_TEMPLATE_LAST};
+        lv_obj_set_grid_dsc_array(tile, column_dsc, row_dsc);
 
-    /*Tile2: a button*/
-    lv_obj_t * tile2 = lv_tileview_add_tile(tv, 0, 1, LV_DIR_TOP | LV_DIR_RIGHT);
+        lv_obj_t * humidity_icon_label = lv_label_create(tile);
+        lv_label_set_text(humidity_icon_label, "\xEF\x9D\x90");
+        lv_obj_set_style_text_font(humidity_icon_label, &FontAwesomePro, 0);
+        lv_obj_set_style_text_align(humidity_icon_label, LV_TEXT_ALIGN_CENTER, 0);
+        lv_obj_set_grid_cell(humidity_icon_label, LV_GRID_ALIGN_STRETCH, 0, 1, LV_GRID_ALIGN_CENTER, 0, 1);
 
-    lv_obj_t * btn = lv_btn_create(tile2);
+        lv_obj_t * humidity_value_label = lv_label_create(tile);
+        lv_label_set_text(humidity_value_label, "N/A%");
+        lv_obj_set_style_text_font(humidity_value_label, &FontAwesomePro, 0);
+        lv_obj_set_style_text_align(humidity_value_label, LV_TEXT_ALIGN_CENTER, 0);
+        lv_obj_set_grid_cell(humidity_value_label, LV_GRID_ALIGN_STRETCH, 1, 1, LV_GRID_ALIGN_CENTER, 0, 1);
 
-    label = lv_label_create(btn);
-    lv_label_set_text(label, "Scroll up or right");
+        lv_obj_t * temperature_icon_label = lv_label_create(tile);
+        lv_label_set_text(temperature_icon_label, "\xEF\x8B\x89");
+        lv_obj_set_style_text_font(temperature_icon_label, &FontAwesomePro, 0);
+        lv_obj_set_style_text_align(temperature_icon_label, LV_TEXT_ALIGN_CENTER, 0);
+        lv_obj_set_grid_cell(temperature_icon_label, LV_GRID_ALIGN_STRETCH, 0, 1, LV_GRID_ALIGN_CENTER, 1, 1);
 
-    lv_obj_set_size(btn, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
-    lv_obj_center(btn);
+        lv_obj_t * temperature_value_label = lv_label_create(tile);
+        lv_label_set_text(temperature_value_label, "N/A℃");
+        lv_obj_set_style_text_font(temperature_value_label, &FontAwesomePro, 0);
+        lv_obj_set_style_text_align(temperature_value_label, LV_TEXT_ALIGN_CENTER, 0);
+        lv_obj_set_grid_cell(temperature_value_label, LV_GRID_ALIGN_STRETCH, 1, 1, LV_GRID_ALIGN_CENTER, 1, 1);
+    }
 
-    /*Tile3: a list*/
-    lv_obj_t * tile3 = lv_tileview_add_tile(tv, 1, 1, LV_DIR_LEFT);
-    lv_obj_t * list = lv_list_create(tile3);
-    lv_obj_set_size(list, LV_PCT(100), LV_PCT(100));
+    {
+        lv_obj_t * tile = lv_tileview_add_tile(tv, 0, 1, LV_DIR_TOP);
+        lv_obj_set_layout(tile, LV_LAYOUT_FLEX);
 
-    lv_list_add_btn(list, NULL, "One");
-    lv_list_add_btn(list, NULL, "Two");
-    lv_list_add_btn(list, NULL, "Three");
-    lv_list_add_btn(list, NULL, "Four");
-    lv_list_add_btn(list, NULL, "Five");
-    lv_list_add_btn(list, NULL, "Six");
-    lv_list_add_btn(list, NULL, "Seven");
-    lv_list_add_btn(list, NULL, "Eight");
-    lv_list_add_btn(list, NULL, "Nine");
-    lv_list_add_btn(list, NULL, "Ten");
+        lv_obj_set_flex_flow(tile, LV_FLEX_FLOW_COLUMN);
+        
+    }
 
 }
 
--- components/stage/lvgl/lv_conf.h
+++ components/stage/lvgl/lv_conf.h
@@ -444,37 +444,37 @@
 
  /*Montserrat fonts with ASCII range and some symbols using bpp = 4
   *https://fonts.google.com/specimen/Montserrat*/
-#define LV_FONT_MONTSERRAT_8  0
-#define LV_FONT_MONTSERRAT_10 0
-#define LV_FONT_MONTSERRAT_12 0
+#define LV_FONT_MONTSERRAT_8  1
+#define LV_FONT_MONTSERRAT_10 1
+#define LV_FONT_MONTSERRAT_12 1
 #define LV_FONT_MONTSERRAT_14 1
-#define LV_FONT_MONTSERRAT_16 0
-#define LV_FONT_MONTSERRAT_18 0
-#define LV_FONT_MONTSERRAT_20 0
-#define LV_FONT_MONTSERRAT_22 0
-#define LV_FONT_MONTSERRAT_24 0
-#define LV_FONT_MONTSERRAT_26 0
-#define LV_FONT_MONTSERRAT_28 0
-#define LV_FONT_MONTSERRAT_30 0
-#define LV_FONT_MONTSERRAT_32 0
-#define LV_FONT_MONTSERRAT_34 0
-#define LV_FONT_MONTSERRAT_36 0
-#define LV_FONT_MONTSERRAT_38 0
-#define LV_FONT_MONTSERRAT_40 0
-#define LV_FONT_MONTSERRAT_42 0
-#define LV_FONT_MONTSERRAT_44 0
-#define LV_FONT_MONTSERRAT_46 0
-#define LV_FONT_MONTSERRAT_48 0
+#define LV_FONT_MONTSERRAT_16 1
+#define LV_FONT_MONTSERRAT_18 1
+#define LV_FONT_MONTSERRAT_20 1
+#define LV_FONT_MONTSERRAT_22 1
+#define LV_FONT_MONTSERRAT_24 1
+#define LV_FONT_MONTSERRAT_26 1
+#define LV_FONT_MONTSERRAT_28 1
+#define LV_FONT_MONTSERRAT_30 1
+#define LV_FONT_MONTSERRAT_32 1
+#define LV_FONT_MONTSERRAT_34 1
+#define LV_FONT_MONTSERRAT_36 1
+#define LV_FONT_MONTSERRAT_38 1
+#define LV_FONT_MONTSERRAT_40 1
+#define LV_FONT_MONTSERRAT_42 1
+#define LV_FONT_MONTSERRAT_44 1
+#define LV_FONT_MONTSERRAT_46 1
+#define LV_FONT_MONTSERRAT_48 1
 
   /*Demonstrate special features*/
-#define LV_FONT_MONTSERRAT_12_SUBPX      0
-#define LV_FONT_MONTSERRAT_28_COMPRESSED 0  /*bpp = 3*/
-#define LV_FONT_DEJAVU_16_PERSIAN_HEBREW 0  /*Hebrew, Arabic, Persian letters and all their forms*/
-#define LV_FONT_SIMSUN_16_CJK            0  /*1000 most common CJK radicals*/
+#define LV_FONT_MONTSERRAT_12_SUBPX      1
+#define LV_FONT_MONTSERRAT_28_COMPRESSED 1  /*bpp = 3*/
+#define LV_FONT_DEJAVU_16_PERSIAN_HEBREW 1  /*Hebrew, Arabic, Persian letters and all their forms*/
+#define LV_FONT_SIMSUN_16_CJK            1  /*1000 most common CJK radicals*/
 
 /*Pixel perfect monospace fonts*/
-#define LV_FONT_UNSCII_8  0
-#define LV_FONT_UNSCII_16 0
+#define LV_FONT_UNSCII_8  1
+#define LV_FONT_UNSCII_16 1
 
 /*Optionally declare custom fonts here.
  *You can use these fonts as default font too and they will be available globally.
@@ -487,10 +487,10 @@
 /*Enable handling large font and/or fonts with a lot of characters.
  *The limit depends on the font size, font face and bpp.
  *Compiler error will be triggered if a font needs it.*/
-#define LV_FONT_FMT_TXT_LARGE 0
+#define LV_FONT_FMT_TXT_LARGE 1
 
  /*Enables/disables support for compressed fonts.*/
-#define LV_USE_FONT_COMPRESSED 0
+#define LV_USE_FONT_COMPRESSED 1
 
 /*Enable subpixel rendering*/
 #define LV_USE_FONT_SUBPX 0
